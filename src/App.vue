fa1263fbcf03e1570298c2d3ef78be36454ede10
beluga <xiandi_shan@outlook.jp>
2021-09-12 19:23:32 +0900

    create repo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <blogHeader></blogHeader>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import blogHeader from './components/header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    blogHeader,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="blog-header">
+      <img src='../assets/walle.png' width="150"/>
+      <h1>The Robots of Dawn</h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'blogHeader'
+}
+</script>
+
+<style scoped>
+.blog-header{
+    font-family: 'Raleway';
+    font-size: 12px;
+    text-align: center;
+    width: 35%;
+    margin-top: 90px;
+}
+</style>
